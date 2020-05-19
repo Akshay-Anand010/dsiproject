a54aec5fed18c2c5127e32b7ab0633149e3cef64
Akshay Anand <teamsmurfo@gmail.com>
2020-05-19 23:37:07 +0530

    latest

--- public/js/login.js
+++ public/js/login.js
@@ -3,7 +3,7 @@ var attempt = 3; // Variable to count number of attempts.
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-if ( username == "ajay" && password == "admin"){
+if ( username == "EEE" && password == "project"){
 alert ("Login successfully");
 window.location = "/home"; // Redirecting to other page.
 return false;
